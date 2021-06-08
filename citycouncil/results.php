9f42c8766248a9ff4614104d2f60c304db660331
Ricardo Alfaro <ricardoalfaro@gmail.com>
2021-06-08 15:58:59 -0400

    Update results.php
    Set the content for the 2021 Primary data feed and made the election and race titles dynamic.

--- citycouncil/results.php
+++ citycouncil/results.php
@@ -1,12 +1,14 @@
 <?php
 
-$data = file_get_contents('https://results.elections.virginia.gov/vaelections/2018%20November%20General/Json/Locality/ALEXANDRIA%20CITY/Index.json');
+$data = file_get_contents('https://results.elections.virginia.gov/vaelections/2021%20June%20Democratic%20Primary/Json/Locality/ALEXANDRIA_CITY/Index.json');
 $json_data = json_decode($data);
 echo('<pre>');
 
-$city_council_array = $json_data->Races[3];
+$city_council_array = $json_data->Races[5];
 echo('</pre>');
 
+$election_name = $json_data->ElectionName;
+$race_name = $city_council_array->RaceName;
 $city_council_candidades = $city_council_array->Candidates;
 $number_of_seats = $city_council_array->NumberOfSeats;
 $precincts_total = $city_council_array->PrecinctsParticipating;
@@ -22,8 +24,8 @@ usort($city_council_candidades, 'sortByOrder');
 ?>
 	<div align="center">
 		<img src="logo.png" style="max-width: 400px; height: auto; padding-bottom: 15px;">
-		<h1>2018 November General: City Council</h1>
-		<h2><?php echo($precincts_reporting.' out of '.$precincts_total.' precincts reporting'); ?></h2>
+		<h2><?php echo($election_name.'<br/>'.$race_name); ?></h2>
+		<h3><?php echo($precincts_reporting.' out of '.$precincts_total.' precincts reporting'); ?></h3>
 	</div>
 	<div class="container">
 		<?php for($i = 0; $i < $number_of_seats; $i++) { ?>
